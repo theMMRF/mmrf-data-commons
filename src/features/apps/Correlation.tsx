5287c2752375d6e80f8a8b8d499293cb8bdccf1c
Gavriel Matt <gmatt@stjude.org>
2026-05-08 12:55:54 -0500

    Implement correlation plot card

---
+++ src/features/apps/Correlation.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { CorrelationWrapper } from "@/features/proteinpaint/CorrelationWrapper";
+
+const CorrelationApp: React.FC = () => {
+  return <CorrelationWrapper />;
+};
+
+export default CorrelationApp;
