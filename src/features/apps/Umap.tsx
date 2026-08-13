23334c424ed57604492f58deb431911d734ee69f
Gavriel Matt <gmatt@stjude.org>
2026-08-13 12:25:24 -0500

    Implement UMAP card

---
+++ src/features/apps/Umap.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { UmapWrapper } from "@/features/proteinpaint/UmapWrapper";
+
+const UmapApp: React.FC = () => {
+  return <UmapWrapper />;
+};
+
+export default UmapApp;
